b52c0c5b084b26a27f44c756ffec289498c54d01
Sam Langberg <slangberg@rentpath.com>
2015-03-11 19:11:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Angular-Reddit-Analytics-Module
---
+++ controllers.js
@@ -0,0 +1,3 @@
+var datamodule = angular.module('datamodule', []);
+
+datamodule.controller('MainController', ['$scope','$q','RedditApi','DataObj','DataApi','Calendar',function($scope,$q,RedditApi,DataObj,DataApi,Calendar){}])//end
